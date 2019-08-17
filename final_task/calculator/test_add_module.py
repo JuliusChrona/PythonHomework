509711193f54528e3e172d180609f5fd9b030367
JuliusChrona <Kirill.vackevich@gmail.com>
2019-08-17 21:23:29 +0300

    update tests

--- final_task/calculator/test_add_module.py
+++ final_task/calculator/test_add_module.py
@@ -1,5 +1,4 @@
 import calculator.module_for_test as tm
-import time
 import unittest
 from calculator.calculation import evaluated
 
@@ -7,9 +6,9 @@ from calculator.calculation import evaluated
 class TestAddModule(unittest.TestCase):
 
     def test_added_module(self):
-        self.assertEqual(evaluated('time()/3600/365/24', ['time']), time.time()/3600/365/24)
         self.assertEqual(evaluated('sin()', ['calculator.module_for_test']), tm.sin())
         self.assertEqual(evaluated('cos()', ['calculator.module_for_test']), tm.cos())
         self.assertEqual(evaluated('pi+e', ['calculator.module_for_test']), tm.pi + tm.e)
         self.assertEqual(evaluated('log(5, 6)', ['calculator.module_for_test']), tm.log(5, 6))
         self.assertEqual(evaluated('log(10, 100)', ['calculator.module_for_test']), tm.log(10, 100))
+        self.assertEqual(evaluated('log(e, pi)', ['calculator.module_for_test']), tm.log(e, pi))
